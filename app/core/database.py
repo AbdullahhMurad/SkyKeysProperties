39af66718d7d3a07c906aec869c96fdd953bf2e5
Abdullah Sufyan Murad <abdullahmurad2002@gmail.com>
2026-06-26 02:41:26 +0400

    Fixed commit errors

--- app/core/database.py
+++ app/core/database.py
@@ -6,9 +6,9 @@ from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from app.core.config import settings
 
 
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 
 # engine = create_engine(settings.DATABASE_URL)
 
