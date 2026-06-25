d4c5dcd1c7c7a56efe043e48ebf48e1bc9ae942c
Abdullah Sufyan Murad <abdullahmurad2002@gmail.com>
2026-06-25 12:17:34 +0400

    Production Database

--- app/core/database.py
+++ app/core/database.py
@@ -1,20 +1,46 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.engine.url import make_url
 
-from app.core.config import settings
+SQLALCHEMY_DATABASE_URL = "postgresql://avnadmin:YOUR_PASSWORD@pg-1f53fee4-theclassicdope1-f1fe.h.aivencloud.com:28191/defaultdb?sslmode=require"
 
+print("=" * 60)
+print("DATABASE.PY LOADED")
 
-SQLALCHEMY_DATABASE_URL = "postgresql://avnadmin:REMOVED_AIVEN_PASSWORD@pg-1f53fee4-theclassicdope1-f1fe.h.aivencloud.com:28191/defaultdb?sslmode=require"
+url = make_url(SQLALCHEMY_DATABASE_URL)
+
+print("HOST =", url.host)
+print("PORT =", url.port)
+print("DATABASE =", url.database)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-# engine = create_engine(settings.DATABASE_URL)
+print("ENGINE CREATED")
+print("=" * 60)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
 class Base(DeclarativeBase):
     pass
+
+
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+# from app.core.config import settings
+
+
+# SQLALCHEMY_DATABASE_URL = "postgresql://avnadmin:REMOVED_AIVEN_PASSWORD@pg-1f53fee4-theclassicdope1-f1fe.h.aivencloud.com:28191/defaultdb?sslmode=require"
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# # engine = create_engine(settings.DATABASE_URL)
+
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# class Base(DeclarativeBase):
+#     pass
 
 
 # ---------------------------------------------------------------------------
