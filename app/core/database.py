62b9cfa7c45c896702cac2ba1dcc6ce28063e433
Abdullah Sufyan Murad <abdullahmurad2002@gmail.com>
2026-06-26 22:19:21 +0400

    Updated CSS and about.html

--- app/core/database.py
+++ app/core/database.py
@@ -7,7 +7,7 @@ from app.core.config import settings
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
 
 # engine = create_engine(settings.DATABASE_URL)
 
